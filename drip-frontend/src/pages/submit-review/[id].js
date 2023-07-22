4c092475a4849ea37071f527369c1a5a95b3730f
Petar Popovic <petarpnp@gmail.com>
2023-07-22 12:56:33 +0200

    Submit review start

---
+++ drip-frontend/src/pages/submit-review/[id].js
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/layout/layout";
+
+const SubmitReview = () => (
+  <MainLayout>
+    <div className="container mx-auto px-4 py-20">
+      Start here
+    </div>
+  </MainLayout>
+);
+
+export default SubmitReview;
