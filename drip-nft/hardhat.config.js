65ae3f3486632a36c4b4ef6002d48e099ff57bb5
Petar Popovic <petarpnp@gmail.com>
2023-07-22 18:26:52 +0200

    Update nfts

--- drip-nft/hardhat.config.js
+++ drip-nft/hardhat.config.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+    },
+    mumbai: {
+      url: "https://blue-broken-river.matic-testnet.discover.quiknode.pro/65fa09d49eda7b1d66a54b2fa11218dd77a0b082/",
+      accounts: {
+        mnemonic: process.env.DEV_MNEMONIC
+      }
+    }
+  },
 };
